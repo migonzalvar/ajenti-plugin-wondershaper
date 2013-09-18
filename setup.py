b92a71f2d7fe62a97048e00bd56cb190b4adf6fc
Miguel Gonzalez <migonzalvar@gmail.com>
2013-09-18 20:16:22 +0200

    Initial version setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from ez_setup import use_setuptools
+use_setuptools()
+
+from setuptools import setup
+
+__version__ = "0.2.1"
+
+
+requires = [r.strip() for r in open('requirements.txt') if r]
+
+
+setup(
+    name='ajenti-plugin-wondershaper',
+    version=__version__,
+    packages=['wondershaper', ],
+
+    install_requires=requires,
+
+    # metadata
+    description='Wondershaper plugin for ajenti',
+    author=u'Miguel González',
+    author_email='migonzalvar@activitycentral.com',
+    url='http://github.com/migonzalvar/ajenti-plugin-wondershper/',
+)
