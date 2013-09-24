92b4b3382404642e1c0b9cfbd5cc6739719e0c25
Miguel Gonzalez <migonzalvar@gmail.com>
2013-09-24 13:42:51 +0200

    Install source code in ajenti plugin path

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import glob
+import os.path
 
 from setuptools import setup
 
@@ -9,13 +11,21 @@ __version__ = "0.2.1"
 
 requires = [r.strip() for r in open('requirements.txt') if r]
 
-
 setup(
     name='ajenti-plugin-wondershaper',
     version=__version__,
-    packages=['wondershaper', ],
-
+    data_files=[
+        (
+            '/var/lib/ajenti/plugins/wondershaper',
+            [f for f in glob.glob(os.path.join('wondershaper', '*py'))]
+        ),
+        (
+            '/var/lib/ajenti/plugins/wondershaper/layout',
+            [f for f in glob.glob(os.path.join('wondershaper', 'layout', '*xml'))]
+        )
+    ],
     install_requires=requires,
+    zip_safe=False,
 
     # metadata
     description='Wondershaper plugin for ajenti',
